a49cf2dc352915b75fca2c00773d464ded29393c
debankanmitra <debankanmitra@gmail.com>
2024-04-16 23:47:17 +0530

    update

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -13,60 +13,140 @@ function Features() {
 						environments.
 					</p>
 
-					<div className="grid gap-2 mt-12 text-center md:grid-cols-3">
+					<div className="grid grid-cols-2 mt-12 text-sm font-medium text-gray-500 gap-x-2 gap-y-12 lg:grid-cols-3 lg:gap-y-16 text-balance text-left">
 						<div>
-							<div className="p-2 overflow-hidden border rounded-3xl">
+							<div className="overflow-hidden rounded-3xl border p-2">
 								<img
 									src="rectangle.svg"
 									alt="#_"
-									className="w-full h-full mx-auto rounded-2xl"
+									className="mx-auto h-full w-full rounded-2xl"
 								/>
 							</div>
-							<div className="mt-4">
-								<p className="font-medium text-gray-900">
-									No warranty disclaimer
-								</p>
-								<p className="mt-2 text-sm text-gray-500">
-									The license comes with no warranties. The licensor provides
-									the work &ldquo;as is,&rdquo; and users must use it at their
-									own risk.
-								</p>
+							<div className="mt-4 flex flex-col justify-between px-4">
+								<div>
+									<p className="text-base text-gray-900">
+										No warranty disclaimer
+									</p>
+									<p className="mt-2">
+										The licensor provides the work &ldquo;as is,&rdquo; and users must use
+										it at their own risk.
+									</p>
+								</div>
+								<div className="mt-8 flex">
+									<a
+										href="#_"
+										className="inline-flex items-center justify-center text-sm font-semibold text-black duration-200 hover:text-blue-500 focus:outline-none"
+										aria-label="Read more about the topic"
+									>
+										<span>Read more</span>
+										<svg
+											aria-hidden="true"
+											focusable="false"
+											className="ml-2 size-4"
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke="currentColor"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												strokeWidth="2"
+												d="M13 7l5 5m0 0l-5 5m5-5H6"
+											></path>
+										</svg>
+									</a>
+								</div>
 							</div>
 						</div>
 						<div>
-							<div className="p-2 overflow-hidden border rounded-3xl">
+							<div className="overflow-hidden rounded-3xl border p-2">
 								<img
 									src="rectangle.svg"
 									alt="#_"
-									className="w-full h-full mx-auto rounded-2xl"
+									className="mx-auto h-full w-full rounded-2xl"
 								/>
 							</div>
-							<div className="px-8 mt-4">
-								<p className="font-medium text-gray-900">
-									Modification Freedom
-								</p>
-								<p className="mt-2 text-sm text-gray-500">
-									You can adapt, remix, transform, and build upon the licensed
-									work.
-								</p>
+							<div className="mt-4 flex flex-col justify-between px-4">
+								<div>
+									<p className="text-base text-gray-900">
+										No warranty disclaimer
+									</p>
+									<p className="mt-2">
+										The licensor provides the work &ldquo;as is,&rdquo; and users must use
+										it at their own risk.
+									</p>
+								</div>
+								<div className="mt-8 flex">
+									<a
+										href="#_"
+										className="inline-flex items-center justify-center text-sm font-semibold text-black duration-200 hover:text-blue-500 focus:outline-none"
+										aria-label="Read more about the topic"
+									>
+										<span>Read more</span>
+										<svg
+											aria-hidden="true"
+											focusable="false"
+											className="ml-2 size-4"
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke="currentColor"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												strokeWidth="2"
+												d="M13 7l5 5m0 0l-5 5m5-5H6"
+											></path>
+										</svg>
+									</a>
+								</div>
 							</div>
 						</div>
 						<div>
-							<div className="p-2 overflow-hidden border rounded-3xl">
+							<div className="overflow-hidden rounded-3xl border p-2">
 								<img
 									src="rectangle.svg"
 									alt="#_"
-									className="w-full h-full mx-auto rounded-2xl"
+									className="mx-auto h-full w-full rounded-2xl"
 								/>
 							</div>
-							<div className="px-8 mt-4">
-								<p className="font-medium text-gray-900">
-									Commercial use allowed
-								</p>
-								<p className="mt-2 text-sm text-gray-500">
-									You are allowed to use the licensed work for both
-									non-commercial and commercial purposes.
-								</p>
+							<div className="mt-4 flex flex-col justify-between px-4">
+								<div>
+									<p className="text-base text-gray-900">
+										No warranty disclaimer
+									</p>
+									<p className="mt-2">
+										The licensor provides the work &ldquo;as is,&rdquo; and users must use
+										it at their own risk.
+									</p>
+								</div>
+								<div className="mt-8 flex">
+									<a
+										href="#_"
+										className="inline-flex items-center justify-center text-sm font-semibold text-black duration-200 hover:text-blue-500 focus:outline-none"
+										aria-label="Read more about the topic"
+									>
+										<span>Read more</span>
+										<svg
+											aria-hidden="true"
+											focusable="false"
+											className="ml-2 size-4"
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke="currentColor"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												strokeWidth="2"
+												d="M13 7l5 5m0 0l-5 5m5-5H6"
+											></path>
+										</svg>
+									</a>
+								</div>
 							</div>
 						</div>
 					</div>
