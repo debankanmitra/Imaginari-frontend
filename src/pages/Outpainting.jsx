b7038a53784a5a510b1da32118560998f490ae3d
debankanmitra <debankanmitra@gmail.com>
2024-04-25 12:43:51 +0530

    outpainting

--- src/pages/Outpainting.jsx
+++ src/pages/Outpainting.jsx
@@ -3,9 +3,15 @@ import { useState } from "react";
 function Outpainting() {
 	const [image, setImage] = useState(null);
 	const [isOpen, setIsOpen] = useState(false);
-	const [selectedOption, setSelectedOption] = useState("BlueWillow v4");
+	const [selectedOption, setSelectedOption] = useState("Stable Diffusion 1.5 - Outpaint");
 
-	const [showGeneratedImage, setShowGeneratedImage] = useState(false);
+	const [file, setFile] = useState(null);
+
+	const [prompt, setPrompt] = useState("");
+	const [negetive_prompt, setNegativePrompt] = useState("");
+	const [url, setUrl] = useState(null);
+
+	const [showGeneratedImage, setShowGeneratedImage] = useState("text");
 
 	const toggleDropdown = () => {
 		setIsOpen(!isOpen);
@@ -19,6 +25,7 @@ function Outpainting() {
 	const handleImageUpload = (event) => {
 		const file = event.target.files[0];
 		const reader = new FileReader();
+		setFile(file)
 
 		reader.onloadend = () => {
 			setImage(reader.result);
@@ -26,6 +33,33 @@ function Outpainting() {
 
 		if (file) {
 			reader.readAsDataURL(file);
+		}
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const formData = new FormData();
+		if (!image) {
+			// Handle error if no file is selected (optional)
+			console.error("Please select an image");
+			return;
+		}
+
+		formData.append("image", file); // imageData is the base64 encoded image
+		formData.append("prompt", prompt);
+		formData.append("negative_prompt", negetive_prompt);
+
+		setShowGeneratedImage("loading");
+		try {
+			const response = await fetch("http://127.0.0.1:8000/outpainting", {
+				method: "POST",
+				body: formData,
+			});
+			const data = await response.json();
+			setUrl(data.output); // Assuming the response contains the URL of the generated image
+			setShowGeneratedImage("image"); // Update state to show the image
+		} catch (error) {
+			console.error("Error uploading image:", error);
 		}
 	};
 	return (
@@ -96,10 +130,10 @@ function Outpainting() {
 															role="menuitem"
 															tabIndex="-1"
 															onClick={() =>
-																handleOptionSelect("BlueWillow v4")
+																handleOptionSelect("Stable Diffusion 1.5 - Outpaint")
 															}
 														>
-															BlueWillow v4
+															Stable Diffusion 1.5 - Outpaint
 														</a>
 														<a
 															href="#"
@@ -109,30 +143,6 @@ function Outpainting() {
 															onClick={() => handleOptionSelect("DALL.E 2")}
 														>
 															DALL.E 2
-														</a>
-													</div>
-													<div className="py-1" role="none">
-														<a
-															href="#"
-															className="text-gray-700 block px-4 py-2 text-sm"
-															role="menuitem"
-															tabIndex="-1"
-															onClick={() =>
-																handleOptionSelect("Stable Diffusion v2")
-															}
-														>
-															Stable Diffusion v2
-														</a>
-														<a
-															href="#"
-															className="text-gray-700 block px-4 py-2 text-sm"
-															role="menuitem"
-															tabIndex="-1"
-															onClick={() =>
-																handleOptionSelect("Google Imagen 2")
-															}
-														>
-															Google Imagen 2
 														</a>
 													</div>
 												</div>
@@ -209,6 +219,7 @@ function Outpainting() {
 											</label>
 											<textarea
 												id="message"
+												onChange={(e) => setPrompt(e.target.value)}
 												rows="4"
 												className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:outline-none focus:ring focus:ring-violet-300"
 												placeholder="Write your thoughts here..."
@@ -229,6 +240,7 @@ function Outpainting() {
 											</label>
 											<textarea
 												id="message"
+												onChange={(e) => setNegativePrompt(e.target.value)}
 												rows="4"
 												className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:outline-none focus:ring focus:ring-violet-300"
 												placeholder="Write your thoughts here..."
@@ -243,6 +255,7 @@ function Outpainting() {
 									<div className="mt-5">
 										<button
 											type="submit"
+											onClick={handleSubmit}
 											className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
 										>
 											Generate
@@ -259,13 +272,14 @@ function Outpainting() {
 										<div className="py-4">
 											{/* <div className="rounded-lg border border-gray-100 bg-white p-4 shadow-lg transition sm:p-6"> */}
 											<div className="block w-full border border-gray-100 p-8 mt-12 bg-white lg:mt-0 rounded-3xl shadow-lg py-20">
-												{showGeneratedImage ? (
+											{showGeneratedImage == "image" && (
 													<img
 														alt="hero"
 														className="object-cover object-center w-72 h-64 mx-auto lg:ml-auto rounded-2xl"
-														src="https://images.unsplash.com/photo-1570129477492-45c003edd2be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+														src={url}
 													/>
-												) : (
+												)}
+												{showGeneratedImage == "text" && (
 													<article className="rounded-lg bg-white p-4 sm:p-6 mx-auto">
 														<span className="inline-block p-2 text-white">
 															<svg
@@ -283,16 +297,20 @@ function Outpainting() {
 														</span>
 														<a href="#">
 															<h3 className="mt-0.5 text-lg font-medium text-gray-900">
-																Generated images will appear here.
+																Animated images will appear here.
 															</h3>
 														</a>
 														<p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
 															Looks like you haven&apos;t created anything yet!
 															On the Left hand template provide sample prompt
-															and then click Generate. Our text-to-image feature
-															turns your words into beautiful AI visuals.
+															and then click Generate. Our image-to-image
+															feature turns your words into beautiful AI
+															visuals.
 														</p>
 													</article>
+												)}
+												{showGeneratedImage == "loading" && (
+													<div className="mx-auto border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600" />
 												)}
 											</div>
 											{/* </div> */}
